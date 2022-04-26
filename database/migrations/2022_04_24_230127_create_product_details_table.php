6c78f0dce38774ee5d7c3c41fadeb8c23b5c0730
Muhammad Syarif Fadli <fadlyknight@gmail.com>
2022-04-26 10:25:14 +0700

    set default stock

--- database/migrations/2022_04_24_230127_create_product_details_table.php
+++ database/migrations/2022_04_24_230127_create_product_details_table.php
@@ -15,7 +15,7 @@ class CreateProductDetailsTable extends Migration
     {
         Schema::create('product_details', function (Blueprint $table) {
             $table->id();
-            $table->integer('stock');
+            $table->integer('stock')->default(0);
             $table->string('summary');
             $table->text('description')->nullable();
             $table->timestamps();
